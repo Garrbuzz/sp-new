bb33132636ad1c09756fa0935185f503fb3053c4
Oleg <garrbuzz@gmail.com>
2019-02-19 14:09:25 +0200

    work login

---
+++ www/js/login.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+	let login = document.querySelector('.btn-login');
+	login.onclick = function(){
+		let name = document.querySelector('#login');
+		let pass = document.querySelector('#pass');
+	 		fetch('./php/login.php', {method:"POST", headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" }, body: "name=" + encodeURIComponent(name.value) + "&pass=" + encodeURIComponent(pass.value)})  
+		  .then(  
+		    function(response) {  
+		      if (response.status !== 200) {  
+		        console.log('Looks like there was a problem. Status Code: ' +  
+		          response.status);  
+		        return;  
+		      }
+
+		      // Examine the text in the response  
+		      response.json().then(function(data) {  
+		        if(data){
+
+		        } else {
+		        	alert('Не правильное имя пользователя или пароль.');
+		        }
+		      });  
+		    }  
+		  )  
+		  .catch(function(err) {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+		
+	};
+
+}
