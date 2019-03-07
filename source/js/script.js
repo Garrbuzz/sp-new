03a9517806c5c32519b627996a368b0baac59229
Oleg <garrbuzz@gmail.com>
2019-03-07 16:00:36 +0200

    work logout

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,8 @@
-window.onload = function(){
-	let sendButton = document.querySelector('#login');
-	sendButton.onclick = function(){
-		var xhr = new XMLHttpRequest();
-	        xhr.open('post', 'http://sptraining/php/ses.php', true);
-	        xhr.send();
-	        if (xhr.status != 200) {
-	          // обработать ошибку
-	          alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-	        } else {
-	          if (xhr.responseText=='true'){
-	          	console(xhr.responseText);	
-	          	document.location.href = 'cabinet.html';
-	          	      	
-	          } else {
-	          	
-	          	alert(xhr.responseText);	
-	          	document.location.href = 'login.html';
-	          	
-	          }
-			}
-	}
-	let logoutButton = document.querySelector('#logout');
-	logoutButton.onclick = function(){
+function logout(){
+		alert('asdasdasd');
 		var xhr = new XMLHttpRequest();
 			body = "type=" + encodeURIComponent('logout');
-	        xhr.open('post', 'http://sptraining/php/login.php', true);
+	        xhr.open('post', 'http://sptraining/php/login.php', false);
 	        xhr.send();
 	        if (xhr.status != 200) {
 	          // обработать ошибку
@@ -34,6 +12,43 @@ window.onload = function(){
 	          	console.log(xhr.responseText);	
 	          	
 			}
+}
+window.onload = function(){
+	let xhr = new XMLHttpRequest();
+	        xhr.open('post', 'http://sptraining/php/ses.php', false);
+	        xhr.send();
+	        if (xhr.status != 200) {
+	          // обработать ошибку
+	          console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+	        } else {
+	          if (xhr.responseText=='true'){
+	          	let butLogout = document.querySelector('.cabinet-logout');	
+	          	console.log(butLogout);
+	          	butLogout.innerHTML = '<button id="logout">выйти</button>';
+	          	      	
+	          } 
+			}	
+	let sendButton = document.querySelector('#login');
+	sendButton.onclick = function(){
+		var xhr = new XMLHttpRequest();
+	        xhr.open('post', 'http://sptraining/php/ses.php', false);
+	        xhr.send();
+	        if (xhr.status != 200) {
+	          // обработать ошибку
+	          console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+	        } else {
+	          if (xhr.responseText=='true'){
+	          		
+	          	document.location.href = 'cabinet.html';
+	          	      	
+	          } else {
+	          	      	
+	          	document.location.href = 'login.html';
+	          	
+	          }
+			}
 	}
+	
+
 	
 }
