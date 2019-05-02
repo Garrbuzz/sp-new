2a7012f0c8f3856bee5aaed7e459b4ca577b1c13
Oleg <garrbuzz@gmail.com>
2019-05-02 22:38:06 +0300

    home

--- www/php/saveResult.php
+++ www/php/saveResult.php
@@ -4,6 +4,7 @@ session_start();
 include 'functions.php';
 $testName = $_POST['testName'];
 $res = $_POST['testRes'];
+$user_id = $_SESSION['user_id'];
 $login = 'registrator';
 $pass = 'MBRXzWJGffVXsERK';
 $pdo = setConnect($login, $pass);
@@ -12,8 +13,9 @@ $pdo = setConnect($login, $pass);
 // $pdo = setConnect('sptraining','GXDj2gx1bNw2Sr27');
 // $login = $_SESSION['user'];
 
-$sql = $pdo->prepare("INSERT INTO TEST_RESULTS (test_id, result) VALUES (:name, :value)");
+$sql = $pdo->prepare("INSERT INTO TEST_RESULTS (test_id, user_id, result) VALUES (:name, :user_id, :value)");
 $sql->bindParam(':name', $testName);
+$sql->bindParam(':user_id', $user_id);
 $sql->bindParam(':value', $res);
 $sql->execute();
 echo json_encode('ok');
