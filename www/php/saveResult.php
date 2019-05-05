067853b78433fbd8c99073231e270807c82c37a5
Oleg <garrbuzz@gmail.com>
2019-05-06 01:32:56 +0300

    home

--- www/php/saveResult.php
+++ www/php/saveResult.php
@@ -1,22 +1,27 @@
 <?php
 // if (!isset($_SESSION)) session_start();
-session_start();
-include 'functions.php';
-$testName = $_POST['testName'];
-$res = $_POST['testRes'];
-$user_id = $_SESSION['user_id'];
-$login = 'registrator';
-$pass = 'MBRXzWJGffVXsERK';
-$pdo = setConnect($login, $pass);
-// $testName = 'qqq';
-// $res = '33';
-// $pdo = setConnect('sptraining','GXDj2gx1bNw2Sr27');
-// $login = $_SESSION['user'];
+echo 'dddd';
+	session_start();
+	include 'functions.php';
+	$testName = $_POST['testName'];
+	$res = $_POST['testRes'];
+	$user_id = $_SESSION['user_id'];
+	$login = 'glukovne_registrator';
+	$pass = 'MBRXzWJGffVXsERK';
+	$pdo = setConnect($login, $pass);
+	$testName = 'qqq';
+	$res = '33';
+	echo 'test ' . $testName;
+	echo 'user ' . $user_id;
+	// $pdo = setConnect('glukovne_sptraining','GXDj2gx1bNw2Sr27');
+	$login = $_SESSION['user'];
 
-$sql = $pdo->prepare("INSERT INTO TEST_RESULTS (test_id, user_id, result) VALUES (:name, :user_id, :value)");
-$sql->bindParam(':name', $testName);
-$sql->bindParam(':user_id', $user_id);
-$sql->bindParam(':value', $res);
-$sql->execute();
-echo json_encode('ok');
+	$sql = $pdo->prepare("INSERT INTO TEST_RESULTS (test_id, user_id, result) VALUES (:name, :user_id, :value)");
+		echo '1';
+	$sql->bindParam(':name', $testName);
+	$sql->bindParam(':user_id', $user_id);
+	$sql->bindParam(':value', $res);
+	$sql->execute();
+
+	echo json_encode('ok');
 ?>
