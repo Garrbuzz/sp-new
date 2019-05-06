ed6c11a228cd97ae24636b8786ea8f6a02e48cc6
Oleg <garrbuzz@gmail.com>
2019-05-07 00:41:47 +0300

    home

--- www/php/saveResult.php
+++ www/php/saveResult.php
@@ -1,6 +1,7 @@
 <?php
 // if (!isset($_SESSION)) session_start();
-echo 'dddd';
+
+
 	session_start();
 	include 'functions.php';
 	$testName = $_POST['testName'];
@@ -9,15 +10,12 @@ echo 'dddd';
 	$login = 'glukovne_registrator';
 	$pass = 'MBRXzWJGffVXsERK';
 	$pdo = setConnect($login, $pass);
-	$testName = 'qqq';
-	$res = '33';
-	echo 'test ' . $testName;
-	echo 'user ' . $user_id;
+	
+
 	// $pdo = setConnect('glukovne_sptraining','GXDj2gx1bNw2Sr27');
 	$login = $_SESSION['user'];
 
 	$sql = $pdo->prepare("INSERT INTO TEST_RESULTS (test_id, user_id, result) VALUES (:name, :user_id, :value)");
-		echo '1';
 	$sql->bindParam(':name', $testName);
 	$sql->bindParam(':user_id', $user_id);
 	$sql->bindParam(':value', $res);
